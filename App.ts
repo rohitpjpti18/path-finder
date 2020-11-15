f87a07301ededd7273abfd3f65a0a9fe47b1e2c0
rohit prajapati <rohitpjpti18@gmail.com>
2020-11-15 17:00:53 +0530

    fixed grid for different display sizes

---
+++ App.ts
@@ -0,0 +1,177 @@
+import { start } from 'repl';
+import Board from './Board';
+import RandomMaze from './mazes/RandomMaze';
+import RandomWeigths from './mazes/RandomWeights';
+import RecursiveDivision from './mazes/RecursiveDivision';
+import * as $ from 'jquery';
+
+
+let temp1 = Math.floor($(document).height()/28)+2;
+let temp2 = Math.floor($(document).width()/28)+5;
+let temp3 = (temp2)+1;
+let temp4 = (temp1-1)*(temp2)-2;
+if (navigator.userAgent.match(/Android/i) 
+        || navigator.userAgent.match(/webOS/i) 
+        || navigator.userAgent.match(/iPhone/i)  
+        || navigator.userAgent.match(/iPad/i)  
+        || navigator.userAgent.match(/iPod/i) 
+        || navigator.userAgent.match(/BlackBerry/i) 
+        || navigator.userAgent.match(/Windows Phone/i)) { 
+        temp1 = 23;
+        temp2 = 15;
+        temp3 = 16;
+        temp4 = 313;
+} 
+let row = temp1;
+let column = temp2;
+const source = temp3;
+const destination = temp4;
+
+// Initialize board
+let board = new Board(row, column, source, destination);
+
+// get find path and clear the board buttons tag
+let startExec = document.getElementById("startExec") as HTMLInputElement;
+let clear = document.getElementById("clear") as HTMLInputElement;
+
+// get all maze buttons tag
+let rdi = document.getElementById("rdi");
+let rdv = document.getElementById("rdv");
+let rdh = document.getElementById("rdh");
+let rwl = document.getElementById("rwl");
+let rweight = document.getElementById("rweight");
+
+
+// get all algorithm buttons tag
+let dfs = document.getElementById("dfs");
+let bfs = document.getElementById("bfs");
+let dijkstra = document.getElementById("dijkstra");
+
+
+let weights = document.getElementById("weights") as HTMLInputElement;
+
+
+// generate random walls by default
+let le = new RandomMaze(board, board.colorHandler);
+le.execute();
+le = null;
+
+
+
+function disableAllButton(){
+    clear.disabled = true;
+    startExec.disabled = true;
+    weights.disabled = true;
+}
+
+function enableAllButton(){
+    clear.disabled = false;
+    startExec.disabled = false;
+    weights.disabled = false;
+}
+
+rdi.onclick = async ()=>{
+    if(clear.disabled){
+        return;
+    }
+    disableAllButton();
+    let rd = new RecursiveDivision(board);
+    await rd.execute(1);
+    enableAllButton();
+}
+
+rdv.onclick = async ()=>{
+    if(clear.disabled){
+        return;
+    }
+    disableAllButton();
+    let rd = new RecursiveDivision(board);
+    await rd.execute(2);
+    enableAllButton();
+}
+
+rdh.onclick = async ()=>{
+    if(clear.disabled){
+        return;
+    }
+    disableAllButton();
+    let rd = new RecursiveDivision(board);
+    await rd.execute(3);
+    enableAllButton();
+}
+
+rwl.onclick = async ()=>{
+    if(clear.disabled){
+        return;
+    }
+    disableAllButton();
+    let rw = new RandomMaze(board, board.colorHandler);
+    rw.execute();
+    enableAllButton();
+}
+
+rweight.onclick = ()=>{
+    if(clear.disabled){
+        return;
+    }
+    disableAllButton();
+    let ri = new RandomWeigths(board, board.colorHandler);
+    ri.execute();
+    enableAllButton();
+}
+
+dfs.onclick = ()=>{
+    //console.log("dfs worked!!");
+    board.algoID = 2;
+}
+
+bfs.onclick = ()=>{
+    board.algoID = 1;
+}
+
+dijkstra.onclick = ()=>{
+    board.algoID = 3;
+}
+
+
+startExec.onclick = ()=>{
+    //console.log("startExec worked!!");
+    board.algoHandler();
+}
+
+clear.onclick = ()=>{
+    board.clearBoard();
+}
+
+
+weights.onclick = () =>{
+    if(weights.checked == true){
+        board.addWeight = true;
+    }else{
+        board.addWeight = false;
+    }
+}
+
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("close");
+
+// When the user clicks the button, open the modal 
+modal.style.display = "block";
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
---
+++ mazes/RecursiveDivision.ts
@@ -0,0 +1,189 @@
+import Board from "../Board";
+import Random from "../utilities/Random";
+
+class RecursiveDivision{
+
+    board:Board;
+    random:Random;
+
+    constructor(board:Board){
+        this.board = board;
+        this.random = new Random();
+
+    }
+
+
+    handleReachability(sor:number, dest:number ){
+        let sourceNeighbours = this.board.nodes.getNeighbours(sor);
+        let destinationNeighbours = this.board.nodes.getNeighbours(dest);
+        console.log(sourceNeighbours);
+        console.log(destinationNeighbours);
+        let isSNAllWalls:boolean = true;
+        let isDNAllWalls:boolean = true; 
+
+        for(let sn = 0; sn<sourceNeighbours.length; sn++){
+            if(!this.board.nodes.isWall(sourceNeighbours[sn])) isSNAllWalls = false;
+        }
+        
+        for(let dn = 0; dn<destinationNeighbours.length; dn++){
+            if(!this.board.nodes.isWall(destinationNeighbours[dn])) isDNAllWalls = false;
+        }
+
+        if(isSNAllWalls){
+            let random = new Random();
+            let i = random.generateRandomNumber(0, sourceNeighbours.length-1);
+            this.board.nodes.setNodeToDefault(sourceNeighbours[i]);
+        }
+
+        if(isDNAllWalls){
+            let random = new Random();
+            let i = random.generateRandomNumber(0, destinationNeighbours.length-1);
+            this.board.nodes.setNodeToDefault(destinationNeighbours[i]);
+        }
+
+        this.board.colorHandler.recolorAllNodes(this.board.nodes.nodeList, this.board.source, this.board.destination);
+    }
+
+    async buildBorder(){
+        for(let i = 0; i<this.board.nodes.column; i++){
+            if(i !== this.board.source && i !== this.board.destination){
+                this.board.nodes.setNodeToWall(i);
+                await this.board.colorHandler.updateNodeColor(this.board.nodes.nodeList[i])
+            }
+        }
+        for(let i = 1; i<this.board.nodes.row-1; i++){
+            if(i*this.board.nodes.column !== this.board.source && i*this.board.nodes.column !== this.board.destination){
+                this.board.nodes.setNodeToWall(i*this.board.nodes.column);
+                this.board.nodes.setNodeToWall(i*this.board.nodes.column+(this.board.nodes.column-1))
+                await Promise.all([this.board.colorHandler.updateNodeColor(this.board.nodes.nodeList[i*this.board.nodes.column]), 
+                this.board.colorHandler.updateNodeColor(
+                    this.board.nodes.nodeList[i*this.board.nodes.column+(this.board.nodes.column-1)])]);
+            }
+        }
+
+        for(let i = 0; i<this.board.nodes.column; i++){
+            if(i+((this.board.nodes.row-1)*this.board.nodes.column) !== this.board.source &&
+               i+((this.board.nodes.row-1)*this.board.nodes.column) !== this.board.destination
+            ){
+                this.board.nodes.setNodeToWall(i+((this.board.nodes.row-1)*this.board.nodes.column));
+                await this.board.colorHandler.updateNodeColor(
+                    this.board.nodes.nodeList[i+((this.board.nodes.row-1)*this.board.nodes.column)]);
+            }
+
+        }
+    }
+
+    async recursiveDivision(rowStartPos:number, rowEndPos:number, colStartPos:number, colEndPos:number){
+        if((rowEndPos>rowStartPos+1)&&(colEndPos>colStartPos+1)){
+            if((rowEndPos-rowStartPos)<(colEndPos-colStartPos)){
+                let divider = this.random.GenerateEvenRandomNumber(colStartPos, colEndPos);
+                for(let i = rowStartPos; i<=rowEndPos; i++){
+                    let currentIndex = this.board.nodes.resolveRowColumn(i, divider);
+                    await this.board.handleWallBuildingHauleHaule(currentIndex);
+                }
+                let openRow = this.random.GenerateOddRandomNumber(rowStartPos, rowEndPos);
+                let openNode = this.board.nodes.resolveRowColumn(openRow, divider);
+                await this.board.handleWallBuildingHauleHaule(openNode);
+
+
+                await this.recursiveDivision(rowStartPos, rowEndPos, colStartPos, divider-1);
+                await this.recursiveDivision(rowStartPos, rowEndPos, divider+1, colEndPos);
+            }
+            else{
+                let divider = this.random.GenerateEvenRandomNumber(rowStartPos, rowEndPos);
+                for(let i = colStartPos; i<=colEndPos; i++){
+                    let currentIndex = this.board.nodes.resolveRowColumn(divider, i);
+                    await this.board.handleWallBuildingHauleHaule(currentIndex);
+                }
+                let openCol = this.random.GenerateOddRandomNumber(colStartPos, colEndPos);
+                let openNode = this.board.nodes.resolveRowColumn(divider, openCol);
+                await this.board.handleWallBuildingHauleHaule(openNode);
+    
+                await this.recursiveDivision(rowStartPos, divider-1, colStartPos, colEndPos);
+                await this.recursiveDivision(divider+1, rowEndPos, colStartPos, colEndPos);
+            }
+        }
+    }
+
+
+    async recursiveDivisionVertical(rowStartPos:number, rowEndPos:number, colStartPos:number, colEndPos:number){
+        if((rowEndPos>rowStartPos+1)&&(colEndPos>colStartPos+1)){
+            let divider = this.random.GenerateEvenRandomNumber(colStartPos, colEndPos);
+            for(let i = rowStartPos; i<=rowEndPos; i++){
+                let currentIndex = this.board.nodes.resolveRowColumn(i, divider);
+                await this.board.handleWallBuildingHauleHaule(currentIndex);
+            }
+            let openRow = this.random.GenerateOddRandomNumber(rowStartPos, rowEndPos);
+            let openNode = this.board.nodes.resolveRowColumn(openRow, divider);
+            await this.board.handleWallBuildingHauleHaule(openNode);
+
+
+            await this.recursiveDivisionVertical(rowStartPos, rowEndPos, colStartPos, divider-1);
+            await this.recursiveDivisionVertical(rowStartPos, rowEndPos, divider+1, colEndPos);
+        }
+    }
+
+
+    async recursiveDivisionHorizontal(rowStartPos:number, rowEndPos:number, colStartPos:number, colEndPos:number){
+        if((rowEndPos>rowStartPos+1)&&(colEndPos>colStartPos+1)){
+            let divider = this.random.GenerateEvenRandomNumber(rowStartPos, rowEndPos);
+                for(let i = colStartPos; i<=colEndPos; i++){
+                    let currentIndex = this.board.nodes.resolveRowColumn(divider, i);
+                    await this.board.handleWallBuildingHauleHaule(currentIndex);
+                }
+                let openCol = this.random.GenerateOddRandomNumber(colStartPos, colEndPos);
+                let openNode = this.board.nodes.resolveRowColumn(divider, openCol);
+                await this.board.handleWallBuildingHauleHaule(openNode);
+    
+                await this.recursiveDivisionHorizontal(rowStartPos, divider-1, colStartPos, colEndPos);
+                await this.recursiveDivisionHorizontal(divider+1, rowEndPos, colStartPos, colEndPos);
+        }
+    }
+
+
+    async recursiveDivisionMaze(){
+        this.board.algoInProgress = true;
+        this.board.clearBoard();
+
+        await this.buildBorder();
+        await this.recursiveDivision(1, this.board.nodes.row-2, 1, this.board.nodes.column-2);
+        
+        this.handleReachability(this.board.source, this.board.destination);
+        this.board.algoInProgress = false;
+    }
+
+
+
+    async recursiveDivisionVerticalMaze(){
+        this.board.algoInProgress = true;
+        this.board.clearBoard();
+
+        await this.buildBorder();
+        await this.recursiveDivisionVertical(1, this.board.nodes.row-2, 1, this.board.nodes.column-2);
+
+        this.board.algoInProgress = false;
+    }
+
+    async recursiveDivisionHorizontalMaze(){
+        this.board.algoInProgress = true;
+        this.board.clearBoard();
+
+        await this.buildBorder();
+        await this.recursiveDivisionHorizontal(1, this.board.nodes.row-2, 1, this.board.nodes.column-2);
+
+        this.board.algoInProgress = false;
+    }
+
+
+
+
+
+    async execute(id:number){
+        if(id == 1) await this.recursiveDivisionMaze();
+        if(id == 2) await this.recursiveDivisionVerticalMaze();
+        if(id == 3) await this.recursiveDivisionHorizontalMaze();
+    }
+}
+
+
+export default RecursiveDivision;
